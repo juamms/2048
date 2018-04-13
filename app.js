6089587b1fe87c36274134d6b3c0555760b9b4dc
Joao Silva <joao.ms94@gmail.com>
2018-04-13 17:34:49 +0000

    Initial implementation of dashboard user backend.

--- app.js
+++ app.js
@@ -12,7 +12,36 @@ const server = http.createServer(app)
 const wsServer = new WebSocket.Server({ server })
 
 var data = {}
+var activePlayers = []
 var archiveData = {}
+var dashboardSocket = null
+
+function updateDashboard() {
+    if (dashboardSocket != null) {
+        let tempData = {}
+        console.log(activePlayers)
+        activePlayers.forEach(p => {
+            tempData[p] = GameHelper.serializeGameData(data[p])
+        });
+
+        let currentData = {
+            current: tempData,
+            archive: archiveData
+        }
+
+        dashboardSocket.send(JSON.stringify(currentData))
+    }
+}
+
+function setupDashboardUser(ws) {
+    ws.on('close', () => {
+        console.log('Disabling dashboard connection.')
+        dashboardSocket = null
+    })
+
+    dashboardSocket = ws
+    updateDashboard()
+}
 
 wsServer.on('connection', (ws, req) => {
     let player = url.parse(req.url, true).query.player
@@ -26,13 +55,24 @@ wsServer.on('connection', (ws, req) => {
         return
     }
 
+    if (player == 'dashbaord') {
+        console.log('New connection from dashboard.')
+        setupDashboardUser(ws)
+
+        return
+    }
+
     console.log('New connection from player', player)
 
     ws.send('Hello ' + player + '!')
 
     data[player] = new GameManager()
+    if (activePlayers.indexOf(player) == -1) {
+        activePlayers.push(player)
+    }
 
     ws.send(GameHelper.serializeGameData(data[player]))
+    updateDashboard()
 
     ws.on('message', (message) => {
         console.log('Received message: %s', message)
@@ -60,14 +100,23 @@ wsServer.on('connection', (ws, req) => {
                 archiveData[player] = []
             }
 
-            archiveData[player].push(GameHelper.serializeGameData(data[player]))
+            let gameData = GameHelper.serializeGameData(data[player])
+            if (archiveData[player].indexOf(gameData) == -1) {
+                archiveData[player].push(gameData)
+            }
         }
 
         ws.send(GameHelper.serializeGameData(data[player]))
+        updateDashboard()
     })
 
     ws.on('close', () => {
         console.log('Deleting data for player %s.', player)
+
+        activePlayers = activePlayers.filter(item => {
+            return item != player
+        })
+
         delete data[player]
     })
 })
